3bfc6a7ad7705d8a1c9264d6bc3d5b6a0cca25fc
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2019-06-12 15:45:52 -0300

    Added csrf meta tag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('img/ico.png') }}">
 
     <title>Odontoradiosis | Home</title>
