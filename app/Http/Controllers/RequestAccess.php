773887f3c632ce31840beb14bc827cd7a1299309
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2020-08-16 15:49:28 -0300

    :recycle: Clean Up

--- app/Http/Controllers/RequestAccess.php
+++ app/Http/Controllers/RequestAccess.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class RequestAccess extends Controller {
-	public function index () {
-		return view('layouts/request');
-	}
+    public function index() {
+        return view('request');
+    }
 }
