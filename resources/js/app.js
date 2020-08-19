7c99ffd7452e981ce96367ad2d17f03fa9af680c
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2020-08-19 04:57:02 -0300

    :scroll: Try to remove security alerts
    I DON'T USE THIS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,13 @@
+require("./bootstrap");
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+/*require("./odontoradiosis/landmark_system/models/OdontoradiosisKeeper");
+require("./odontoradiosis/landmark_system/util/UsefulMethods");
+require("./odontoradiosis/landmark_system/util/ScaleManager");
+require("./odontoradiosis/landmark_system/views/Canvas");
+require("./odontoradiosis/landmark_system/views/AnatomicalTracing");
+require("./odontoradiosis/landmark_system/controllers/ImageEffects");
+require("./odontoradiosis/landmark_system/controllers/subcontrollers/TracingController");
+require("./odontoradiosis/landmark_system/controllers/subcontrollers/LandmarksController");
+require("./odontoradiosis/landmark_system/controllers/MainController");
+require("./odontoradiosis/landmark_system/events/EventsController");
+require("./odontoradiosis/landmark_system/main");*/
