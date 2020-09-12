1ff6bbaffa563005eeec0c36ccd994550c0cdde8
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2020-09-12 18:56:12 -0300

    :snail:  Time to use Deno+Drash
    Bye Laravel, you was beautiful for some time

--- app/Http/Controllers/RequestAccess.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-class RequestAccess extends Controller {
-    public function index() {
-        return view('request');
-    }
-}
---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { Drash } from "https://deno.land/x/drash@v1.2.2/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.63.0/testing/asserts.ts";
