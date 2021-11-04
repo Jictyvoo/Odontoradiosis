0d99dcb850c4279587a38a0b0519fbdcc2191234
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2021-11-03 23:09:09 -0300

    :package: :art: dependency: Add `Angular Material` as dependency
    Add Material Modules into AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelpPageComponent } from './help-page/help-page.component';
@@ -8,8 +15,27 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-    declarations: [AppComponent, HelpPageComponent, SidebarComponent, ToolbarComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        // Custom Components
+        AppComponent,
+        HelpPageComponent,
+        SidebarComponent,
+        ToolbarComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+
+        // Angular Material Components
+        MatSelectModule,
+        MatIconModule,
+        MatSliderModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
