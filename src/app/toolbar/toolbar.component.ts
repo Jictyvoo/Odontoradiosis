083d91164b40d7f31df21921ea42ed322bf63a46
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2021-11-04 00:21:11 -0300

    :ship: :iphone: enhancements: Refactor toolbar
    - Replace button images by material icons
    - Create buttons as raisedButton

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+    selector: 'app-toolbar',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    openImageSelection(): void {
+        console.debug('Open image selection');
+    }
 
+    openWindowSave(): void {
+        console.log('Open window save');
+    }
+
+    markSemiautomatic(): void {
+        console.log('Mark semiautomatic');
+    }
+
+    exportImage(): void {
+        console.log('Export image');
+    }
 }
