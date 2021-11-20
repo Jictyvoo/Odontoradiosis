6ff826d111662d549e38fa29e5d155a918e50c36
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2021-11-20 02:26:25 -0300

    Auto-generated commit [ci skip]

--- cephalometric-canvas/cephalometric-canvas.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="cephalometric-canvas" />
-export * from './public-api';
--- cephalometric-canvas/lib/domain/util/interfaces/repositories.d.ts
+++
@@ -1,5 +0,0 @@
-export interface ILocalRepository {
-    get<T>(key: string): T | null;
-    set<T>(key: string, value: T): void;
-    remove(key: string): void;
-}
