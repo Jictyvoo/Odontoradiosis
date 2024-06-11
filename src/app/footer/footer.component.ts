a12a9c9411404d0fba590392ce0d8a85d88ebc61
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-06-11 00:34:15 -0300

    :calendar: style: Add current year to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
     selector: 'app-footer',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-    constructor() {}
 
-    ngOnInit(): void {}
+    private now: Date;
+
+    constructor() {
+        this.now = new Date();
+    }
+
+    ngOnInit(): void {
+    }
+
+    get currentDate(): number {
+        const lastYearEdition = 2024;
+
+        const currentYear = this.now.getFullYear();
+        return Math.max(lastYearEdition, currentYear);
+    }
 }
